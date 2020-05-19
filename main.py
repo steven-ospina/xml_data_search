cb5f05b278d20f83b9464b9c0531cd4c80b4d6fe
steven <stevensocio@gmail.com>
2020-05-18 23:37:39 -0500

    proyect refactoring with xml library integration

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from modules.searchData import Data
 
+
 def run():
+    message = Data().print_message()
     menu = Data().menu()
 
 if __name__ == '__main__':
-    print('B I E N V E N I D O\n')
-    print('Nota: verifica que en directorio del proyecto estén los archivos:')
-    print('[*.csv y *.xml]')
-    print('[Los datos del csv deben estar en una sola columna y solo numeros]')
-    print('[cargar un archivo csv y xml a la ves]\n')
     run()
--- modules/buildXml.py
+++ modules/buildXml.py
@@ -1,22 +1,10 @@
-from searchData import Data
+from .searchData import Data
+
 
 class Build(Data):
     # Initializer / Instance Attributes
     def __init__(self):
-        # super().__init__()
+        super().__init__(s)
         Data.__init__(self)
-        # self.archive_xml = archive_Xml
-        # self.list_data_csv = list_data_csv
-        self.reading_method = 'r'
-        self.data_clean = []
-        self.final_data = []
-        self.count = 0
-        self.count2 = 0
-        self.flag = True
-        self.account_statements = ''
-        self.obligation = ''
     # instance method
-    def getirectory(self):
-        hola = super().self.get_the_current_working_directory()
-        print(hola)
 
